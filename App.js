12291a93e095df4dc453ccb5afed7a5dcad88813
caapppaa <145199409+caapppaa@users.noreply.github.com>
2024-03-26 22:17:42 +1100

    Adding Guitars Functionality
    You can now add a guitar to the list, it also has appropriate styling

--- App.js
+++ App.js
@@ -1,27 +1,73 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGuitarText, setEnteredGuitarText] = useState("");
+  const [guitar, setGuitars] = useState([]);
+
+  function specGuitarHandler(enteredText) {
+    setEnteredGuitarText(enteredText);
+  }
+
+  function addGuitarHandler() {
+    setGuitars((currentGuitars) => [...currentGuitars, enteredGuitarText]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Hello World!</Text>
-      <Text>Another Piece of Text</Text>
-      <Button title='Tap Me!' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add Your guitars!"
+          onChangeText={specGuitarHandler}
+        />
+        <Button title="Add Guitar" onPress={addGuitarHandler} />
+      </View>
+
+      <View style={styles.guitarsContainer}>
+        <Text>My Collection</Text>
+        {guitar.map((guitar) => (
+          <View key={guitar} style={styles.guitarItem}>
+            <Text style={styles.guitarText}> {guitar} </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 16,
   },
-  hello: {
-    fontSize: 23, 
-    margin: 10, 
-    borderWidth: 2, 
-    borderColor: 'blue', 
-    padding: 10
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    BorderBottomColour: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  guitarsContainer: {
+    flex: 5,
+  },
+  guitarItem: {
+    margin: 3,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#161b22"
+  },
+  guitarText: {
+    color: "white",
   }
 });
